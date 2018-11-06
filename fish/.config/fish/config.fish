551b6d64c3b6413b3fe69a582165583194780e85
León Illanes <lillanes@cs.toronto.edu>
2018-11-06 17:40:02 -0500

    fish: better vi-mode, better C-c, working delete key

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,7 @@
 fish_vi_key_bindings
 alias :e vim
 alias :q exit
+set -g fish_escape_delay_ms 10
 
 # Disable greeting
 set fish_greeting
@@ -10,5 +11,12 @@ set fish_greeting
 set -g fish_prompt_pwd_dir_length 0
 
 function fish_user_key_bindings
+    # C-c clears command line instead of breaking and starting a new one, this
+    # works well with multiline prompts (default behavior does not):
+    bind -M default \cc 'commandline ""'
     bind -M insert \cc 'commandline ""'
+    bind -M replace \cc 'commandline ""'
+    bind -M visual \cc 'commandline ""'
+    # Workaround for usable delete key in st:
+    bind -M insert \e\[P delete-char
 end
