d9578d6f66a6688348198d1482969f86a27a3479
León Illanes <lillanes@cs.toronto.edu>
2018-12-06 15:28:17 -0500

    zsh: cleaner way of handling colors

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,1 +1,4 @@
-eval $(dircolors -b)
+autoload colors; colors
+
+# setup LS_COLORS environment variable (if unset)
+[ -z "$LS_COLORS" ] && eval $(dircolors -b)
--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -12,7 +12,6 @@ zstyle ':completion:*' completer _expand _complete _correct _approximate
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # Display colors for completion menu
-eval $(dircolors -b)
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 # Highlight selected item in menu
