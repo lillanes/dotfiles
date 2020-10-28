92b024306144c7c8bc1e517a99bba8fc8f560a1d
León Illanes <lillanes@cs.toronto.edu>
2020-10-28 12:03:31 -0300

    zsh: Print error codes

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,3 +1,8 @@
 # spelling correction for commands and parameters
 setopt correct
 setopt correctall
+
+# Print error exit codes. Set as an event triggered after exit of the current
+# function, because print_exit_value gets restored on exit (and we are
+# currently inside an anonymous function defined in zshrc).
+trap 'setopt print_exit_value' EXIT
